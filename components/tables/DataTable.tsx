00ca58457af3be506d4eef61f670fad937d4810e
Jacky LIM <j4cky.lm@gmail.com>
2022-12-23 20:30:01 +0100

    feat: useless mock

--- components/tables/DataTable.tsx
+++ components/tables/DataTable.tsx
@@ -1,4 +1,4 @@
-import { TableContainer, TableHead, TableRow, TableCell, TableBody, Table, Pagination } from "@mui/material";
+import { TableContainer, TableHead, TableRow, TableCell, TableBody, Table } from "@mui/material";
 import { FC } from "react";
 import style from '../../styles/Table.module.css';
 
@@ -11,24 +11,6 @@ interface DataInfo {
 interface TableProps {
   data: DataInfo[];
 }
-
-function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number,
-) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
 
 export const DataTable: FC<TableProps> = ({ data }) => {
   return (
