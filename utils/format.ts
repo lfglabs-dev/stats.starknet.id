bf370bd728585ac1ee496606b13033dad9cb7e5d
Jacky LIM <j4cky.lm@gmail.com>
2023-01-10 19:45:32 +0100

    feat: format util

--- utils/format.ts
+++ utils/format.ts
@@ -3,3 +3,8 @@ export const formatValue = (value: number, fractionDigits = 2) => new Intl.Numbe
   notation: 'compact',
   compactDisplay: 'short'
 }).format(value)
+
+export const formatDomain = (domain: string) => {
+  const cleanedDomain = domain.replace('.stark', '');
+  return cleanedDomain;
+}
