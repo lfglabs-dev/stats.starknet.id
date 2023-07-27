d36e2d3447d40a5f0b19c4ec5600af7673736730
Jacky LIM <j4cky.lm@gmail.com>
2023-07-27 20:00:08 +0200

    feat: main stat card

---
+++ components/dataComponents/MainStatCards.tsx
@@ -0,0 +1,40 @@
+import { FC } from "react";
+import { Period, PeriodRange } from "../../types/metrics";
+import styles from "../../styles/Home.module.css";
+import { StatCard } from "../cards/StatCard";
+import { useGetDomains, useGetIdentities, useGetUniqueAddresses } from "../../hooks/metrics";
+
+
+interface MainStatCardsProps {
+  period: Period;
+  periodRange: PeriodRange;
+}
+
+export const MainStatCards: FC<MainStatCardsProps> = ({ period, periodRange }) => {
+  const { domainsCreated, isLoading: domainIsLoading } = useGetDomains({
+    periodRange,
+    period,
+  });
+
+  const { identitiesCreated, isLoading: identitiesIsLoading } = useGetIdentities({
+    periodRange,
+    period,
+  });
+
+  const { uniqueAddresses, isLoading: uniqueAddressIsLoading } = useGetUniqueAddresses({
+    periodRange,
+    period,
+  });
+
+  return (
+    <div className={styles.row}>
+      <StatCard title="Domains created" statValue={domainsCreated ?? 0}  isLoading={domainIsLoading} />
+      <StatCard
+        title="Identities created"
+        statValue={identitiesCreated ?? 0}
+        isLoading={identitiesIsLoading}
+      />
+      <StatCard title="Unique addresses" statValue={uniqueAddresses ?? 0} isLoading={uniqueAddressIsLoading} />
+    </div>
+  )
+}
